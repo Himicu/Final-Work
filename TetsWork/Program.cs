dd81aac78074fa034881fcb223b1b6c17b5b7e88
hizumi <nyatel@yandex.ru>
2023-06-19 14:08:51 +0400

    Solution with creating a new array

--- TetsWork/Program.cs
+++ TetsWork/Program.cs
@@ -1,32 +1,45 @@
-﻿void CreateArray(string[] stringArray){
+﻿void CreateArray(string[] stringArray)
+{
     for (int i = 0; i < stringArray.Length; i++)
     {
-        stringArray[i] = Console.ReadLine();   
+        stringArray[i] = Console.ReadLine();
     }
 }
-void PrintAray(string[] stringArray){
+void PrintAray(string[] stringArray)
+{
     for (int i = 0; i < stringArray.Length; i++)
     {
         Console.Write($"{stringArray[i]} ");
     }
 }
-void SearchFor3Char(string[] stringArray){
+int SearchSize3Char(string[] stringArray)
+{
     int count = 0;
     for (int i = 0; i < stringArray.Length; i++)
     {
-        count = 0;
-       for (int j = 0; j < stringArray[i].Length; j++)
-       {
-         count++;
-       }
-       if(count <= 3){
-        Console.Write($"{stringArray[i]} ");
-       }
+        if (stringArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void CreateNewArray(string[] oldArray, string[] newArray)
+{
+    for (int i = 0, j = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= 3)
+        {
+            newArray[j] = oldArray[i];
+            j++;
+        }
     }
 }
 string[] stringArray = new string[5];
-System.Console.WriteLine("Введите 5 слов:");
+Console.WriteLine("Введите 5 слов:");
 CreateArray(stringArray);
 PrintAray(stringArray);
-System.Console.WriteLine();
-SearchFor3Char(stringArray);
+Console.WriteLine();
+string[] newStringArray = new string[SearchSize3Char(stringArray)];
+CreateNewArray(stringArray, newStringArray);
+PrintAray(newStringArray);
